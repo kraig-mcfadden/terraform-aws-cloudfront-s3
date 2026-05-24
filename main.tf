55df31d8f2260f5fc7e79931260180853dd1e165
Kraig McFadden <kmcfadden95@gmail.com>
2026-05-23 20:04:28 -0400

    Make module work for serving static assets, not just websites (#1)

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+locals {
+  is_website = var.mode == "website"
+  origin_id  = "primary"
+}
+
 /* ------- S3 ------- */
 
 resource "aws_s3_bucket" "artifact_bucket" {
@@ -12,7 +17,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "artifact_bucket_s
       sse_algorithm = "AES256"
     }
 
-    bucket_key_enabled = "true"
+    bucket_key_enabled = true
   }
 }
 
@@ -49,9 +54,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "artifact_bucket_lifecycle" {
 
 resource "aws_s3_bucket_public_access_block" "allow_public_access" {
   bucket = aws_s3_bucket.artifact_bucket.id
+
+  block_public_acls       = !local.is_website
+  block_public_policy     = !local.is_website
+  ignore_public_acls      = !local.is_website
+  restrict_public_buckets = !local.is_website
 }
 
-data "aws_iam_policy_document" "bucket_policy_doc" {
+data "aws_iam_policy_document" "website_bucket_policy_doc" {
+  count = local.is_website ? 1 : 0
+
   statement {
     sid       = "PublicReadGetObject"
     effect    = "Allow"
@@ -64,14 +76,36 @@ data "aws_iam_policy_document" "bucket_policy_doc" {
   }
 }
 
+data "aws_iam_policy_document" "oac_bucket_policy_doc" {
+  count = local.is_website ? 0 : 1
+
+  statement {
+    sid       = "AllowCloudFrontServicePrincipalRead"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.artifact_bucket.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.distro.arn]
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "artifact_bucket_policy" {
   bucket = aws_s3_bucket.artifact_bucket.bucket
-  policy = data.aws_iam_policy_document.bucket_policy_doc.json
+  policy = local.is_website ? data.aws_iam_policy_document.website_bucket_policy_doc[0].json : data.aws_iam_policy_document.oac_bucket_policy_doc[0].json
 
   depends_on = [aws_s3_bucket_public_access_block.allow_public_access]
 }
 
 resource "aws_s3_bucket_website_configuration" "artifact_bucket_website_config" {
+  count = local.is_website ? 1 : 0
+
   bucket = aws_s3_bucket.artifact_bucket.bucket
 
   index_document {
@@ -81,29 +115,51 @@ resource "aws_s3_bucket_website_configuration" "artifact_bucket_website_config" 
 
 /* ------- Cloudfront ------- */
 
-resource "aws_cloudfront_distribution" "distro" {
-  origin {
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
-    }
+resource "aws_cloudfront_origin_access_control" "this" {
+  count = local.is_website ? 0 : 1
 
-    domain_name = aws_s3_bucket_website_configuration.artifact_bucket_website_config.website_endpoint
-    origin_id   = aws_s3_bucket_website_configuration.artifact_bucket_website_config.website_endpoint
+  name                              = "${var.name}-oac"
+  description                       = "OAC for ${var.name} object bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "distro" {
+  dynamic "origin" {
+    for_each = local.is_website ? [1] : []
+    content {
+      domain_name = aws_s3_bucket_website_configuration.artifact_bucket_website_config[0].website_endpoint
+      origin_id   = local.origin_id
+
+      custom_origin_config {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "http-only"
+        origin_ssl_protocols   = ["TLSv1.2"]
+      }
+    }
+  }
+
+  dynamic "origin" {
+    for_each = local.is_website ? [] : [1]
+    content {
+      domain_name              = aws_s3_bucket.artifact_bucket.bucket_regional_domain_name
+      origin_id                = local.origin_id
+      origin_access_control_id = aws_cloudfront_origin_access_control.this[0].id
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  default_root_object = "index.html"
+  default_root_object = local.is_website ? "index.html" : null
 
   aliases = var.aliases
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket_website_configuration.artifact_bucket_website_config.website_endpoint
+    target_origin_id = local.origin_id
 
     forwarded_values {
       query_string = false
@@ -114,9 +170,9 @@ resource "aws_cloudfront_distribution" "distro" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    min_ttl                = var.min_ttl
+    default_ttl            = var.default_ttl
+    max_ttl                = var.max_ttl
   }
 
   price_class = "PriceClass_100"
